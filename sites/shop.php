3ac967ab1a805f4e91ac7bb0aebdb4ea6d6d7db6
xamika <jajajajakil@hotmail.ch>
2015-05-18 11:23:27 +0200

    Revert "added a Filter button"
    This reverts commit dca353c0129fb4b44bfccf4894213e5276cc72e0.

--- sites/shop.php
+++ sites/shop.php
@@ -1,62 +1,53 @@
-<div class="row" xmlns="http://www.w3.org/1999/html">
+<div class="row">
     <div class="col-lg-12">
         <h1 class="page-header">Shop</h1>
+		<h4>Filter:</h4>
+		<form method="get" action="index.php">
+			<input type="hidden" name="site" value="shop">
+			<input type="text" name="suche" class="form-control col-lg-4 col-sm-6" placeholder="Suche nach Name" />
+			<br />
     </div>
-        <a class="btn btn-lg btn-primary" data-toggle="collapse" href="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-            Filter:
-        </a>
-        <a class="btn btn-lg btn-primary" href="index.php?site=shop">
-            Filter löschen
-        </a>
-        <div class="collapse" id="collapseExample">
-            <div class="col-lg-12">
-            <h4>Filter:</h4>
-            <form method="get" action="index.php">
-            <input type="hidden" name="site" value="shop">
-            <input type="text" name="suche" class="form-control col-lg-4 col-sm-6" placeholder="Suche nach Name" />
-            <br />
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <h5>Platform:</h5>
-            <input type="hidden" name="site" value="shop" >
-            <?php
-                require_once("sql/select_games.php");
-                while($platform = $result_all_platforms->fetch_assoc()) {
-           ?>
-                    <div class="radio">
-                        <label>
-                            <input type="radio" name="platform" id="platform" value=<?php echo $platform["id"] ?>>
-                            <?php
-                                echo $platform["name"]
-                            ?>
-                        </label>
-                    </div>
-           <?php
-                } //End while platform
-           ?>
-        </div>
-        <div class="col-lg-4 col-sm-6">
-            <h5>Kategorie:</h5>
-            <?php
-                while($kategorie = $result_all_kategories->fetch_assoc()) {
-            ?>
-                    <div class="radio">
-                        <label>
-                            <input type="radio" name="kategorie" id="kategorie" value=<?php echo $kategorie["id"] ?>>
-                            <?php
-                                echo $kategorie["name"]
-                            ?>
-                            </label>
-                    </div>
-            <?php
-                } //end while kategorie
-            ?>
-        </div>
-        <div class="col-lg-12">
-            <input type="submit" name="submit" value="Suchen", class="btn btn-lg btn-primary btn-block" />
-            </form> <!-- Ende des forms für die Filter -->
-        </div>
-    </div>
+	<div class="col-lg-4 col-sm-6">
+		<h5>Platform:</h5>
+			<input type="hidden" name="site" value="shop" >
+			<?php 
+				require_once("sql/select_games.php"); 
+				while($platform = $result_all_platforms->fetch_assoc()) {
+			?>
+			<div class="radio">
+				<label>
+					<input type="radio" name="platform" id="platform" value=<?php echo $platform["id"] ?>>
+					<?php
+						echo $platform["name"]
+					?>
+				</label>
+			</div>
+			<?php 
+				} //End while platform
+			?>
+	</div>
+	<div class="col-lg-4 col-sm-6">
+			<h5>Kategorie:</h5>
+			<?php
+				while($kategorie = $result_all_kategories->fetch_assoc()) {
+			?>
+			<div class="radio">
+				<label>
+					<input type="radio" name="kategorie" id="kategorie" value=<?php echo $kategorie["id"] ?>>
+					<?php
+						echo $kategorie["name"]
+					?>
+				</label>
+			</div>
+			<?php
+				} //end while kategorie
+			?>
+	</div>
+	<div class="col-lg-12">
+			<input type="submit" name="submit" value="Filter", class="btn btn-lg btn-primary btn-block" />
+		</form>
+	</div>
+	
 </div>
 <div class="row">
 	<div class="col-lg-12">
