90b1a17d97f656e20b80fd8710f22b3c6e586783
unknown <eeglit@WS-INF-STUD-17.bbcnet.ch>
2015-06-08 16:48:51 +0200

    impressum hinzugefügt

---
+++ sites/impressum.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">
+            Impressum
+        </h1>
+       <h3>OWGS AG</h3>
+        <h4>
+            Adresse: <br />
+            OWGSStreet 22 <br />
+            3333 OWGS <br />
+            email: support@owgs.esy.es
+        </h4>
+    </div>
+</div>
